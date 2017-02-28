63b0c8e51cdfab914be53fc77d65f4120b5112d9
Elena Santana <fanelenan7@gmail.com>
2017-02-27 19:26:07 -0500

    about to start messing with Album class

--- db/migrate/20170227135215_create_comments.rb
+++ db/migrate/20170227135215_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.string :text
-      t.string :submission_id
-      t.string :user_id
+      t.integer :submission_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227135548) do
+ActiveRecord::Schema.define(version: 20170227234407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string   "text"
-    t.string   "submission_id"
-    t.string   "user_id"
+    t.integer  "submission_id"
+    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
