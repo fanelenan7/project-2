ba5422011e0e6d679a8e5d8ce555a6ab70245e57
Elena Santana <fanelenan7@gmail.com>
2017-03-01 16:55:35 -0500

    cleaned up the code

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,32 +1,43 @@
 class AlbumsController < ApplicationController
+
   def index
     @albums = Album.all
   end
+
   def show
     @album = Album.find(params[:id])
   end
+
   def edit
     @album = Album.find(params[:id])
   end
+
   def new
     @album = Album.new
   end
+
   def create
     @album = Album.create!(album_params.merge(user: current_user))
+    # redirect to root because it feels more user-friendly than just stare at an empty album
     redirect_to "/"
   end
+
   def update
     @album = Album.find(params[:id])
     @album.update(album_params.merge(user:current_user))
     redirect_to album_path(@album)
   end
+
   def destroy
     @album = Album.find(params[:id])
     @album.destroy
+    # redirect to root because there is no index page for albums
     redirect_to "/"
   end
+
   private
   def album_params
     params.require(:album).permit(:title)
   end
+  
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,38 +1,46 @@
 class SubmissionsController < ApplicationController
+
   def index
     @albums = Album.all
     @submissions = Submission.all
   end
+
   def show
     @submission = Submission.find(params[:id])
-    # @album = Album.find(params[:album_id])
   end
+
   def edit
     @submission = Submission.find(params[:id])
     @albums = Album.all
   end
+
   def new
     @album = Album.new
     @submission = Submission.new
   end
+
   def create
     @submission = Submission.create!(submission_params.merge(user: current_user))
     @submission.save
     redirect_to submission_path(@submission)
   end
+
   def update
     @submission = Submission.find(params[:id])
     @submission.update!(submission_params.merge(user:current_user))
     @submission.save
     redirect_to submission_path(@submission)
   end
+
   def destroy
     @submission = Submission.find(params[:id])
     @submission.destroy
     redirect_to submissions_path
   end
+
   private
   def submission_params
     params.require(:submission).permit(:title, :url, :description, :album_id)
   end
+
 end
